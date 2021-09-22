2a3c204acaee757d487f05a3260ce0c75c228911
Kristyn Smith <ksmit109@xula.edu>
2021-09-22 14:41:05 +0000

    half-finished GreetMachine object

---
+++ GreetMachine.java
@@ -0,0 +1,17 @@
+public class GreetMachine {
+    
+    private String DEFAULT_NAME = "Earthlings";
+    private String name;
+    
+    public GreetMachine() {
+        name = DEFAULT_NAME;
+    }
+    
+    public GreetMAchine(String name_in) {
+        name = name_in;
+    }
+    
+    public String toString() {
+        
+    }
+}
