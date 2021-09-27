70ad1338903d080b0423ea73fd46f1256698bc89
Kristyn Smith <ksmit109@xula.edu>
2021-09-27 14:05:52 +0000

    fixed default

--- Greeter.java
+++ Greeter.java
@@ -2,7 +2,7 @@ public class Greeter {
     public static void main(String[] args) {
         
         String nameToGreet = 
-            args.length > 0 ? args[0] : "people of planet Earth";
+            args.length > 0 ? args[0] : "Residents of Earth";
         
         System.out.printf("Greetings, %s!\n", nameToGreet);
     }
